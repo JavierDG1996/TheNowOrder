efa5b33f03b2485b449b50105ab8d72f1589ab29
Javier Duran <44509655+JavierDG1996@users.noreply.github.com>
2019-12-03 01:34:30 +0100

    Añadido color de fondo en la lista de productos de un pedido.
    Añadido fragmento de información de autores.

--- app/src/main/java/infilms/asee/giiis/unex/es/thenoworder/ui/send/SendFragment.java
+++ app/src/main/java/infilms/asee/giiis/unex/es/thenoworder/ui/send/SendFragment.java
@@ -22,11 +22,39 @@ public class SendFragment extends Fragment {
         sendViewModel =
                 ViewModelProviders.of(this).get(SendViewModel.class);
         View root = inflater.inflate(R.layout.fragment_send, container, false);
-        final TextView textView = root.findViewById(R.id.text_send);
-        sendViewModel.getText().observe(this, new Observer<String>() {
+        final TextView main_text_about = root.findViewById(R.id.text_about);
+        final TextView author_name1 = root.findViewById(R.id.text_Author1_name);
+        final TextView author_gmail1 = root.findViewById(R.id.text_Author1_gmail);
+        final TextView author_name2 = root.findViewById(R.id.text_Author2_name);
+        final TextView author_gmail2 = root.findViewById(R.id.text_Author2_gmail);
+        sendViewModel.getAbout().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                main_text_about.setText(s);
+            }
+        });
+        sendViewModel.getAuthor_name_1().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                author_name1.setText(s);
+            }
+        });
+        sendViewModel.getAuthor_gmail_1().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                author_gmail1.setText(s);
+            }
+        });
+        sendViewModel.getAuthor_name_2().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                author_name2.setText(s);
+            }
+        });
+        sendViewModel.getAuthor_gmail_2().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                author_gmail2.setText(s);
             }
         });
         return root;
--- app/src/main/java/infilms/asee/giiis/unex/es/thenoworder/ui/send/SendViewModel.java
+++ app/src/main/java/infilms/asee/giiis/unex/es/thenoworder/ui/send/SendViewModel.java
@@ -6,14 +6,46 @@ import androidx.lifecycle.ViewModel;
 
 public class SendViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
+    private MutableLiveData<String> mTextAbout;
+    private MutableLiveData<String> mTextAuthor1;
+    private MutableLiveData<String> mTextGmail1;
+    private MutableLiveData<String> mTextAuthor2;
+    private MutableLiveData<String> mTextGmail2;
 
     public SendViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("Opción próximamente disponible");
+        mTextAbout = new MutableLiveData<>();
+        mTextAbout.setValue("Esta aplicación fue creada por:");
+
+        mTextAuthor1 = new MutableLiveData<>();
+        mTextAuthor1.setValue("Javier Durán García");
+
+        mTextGmail1 = new MutableLiveData<>();
+        mTextGmail1.setValue("jadurang@alumnos.unex.es");
+
+        mTextAuthor2 = new MutableLiveData<>();
+        mTextAuthor2.setValue("Juan Francisco Martín");
+
+        mTextGmail2 = new MutableLiveData<>();
+        mTextGmail2.setValue("jfmartin@alumnos.unex.es");
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<String> getAbout() {
+        return mTextAbout;
+    }
+
+    public LiveData<String> getAuthor_name_1() {
+        return mTextAuthor1;
+    }
+
+    public LiveData<String> getAuthor_gmail_1() {
+        return mTextGmail1;
+    }
+
+    public LiveData<String> getAuthor_name_2() {
+        return mTextAuthor2;
+    }
+
+    public LiveData<String> getAuthor_gmail_2() {
+        return mTextGmail2;
     }
 }
